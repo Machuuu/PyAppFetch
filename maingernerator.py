622a564a4b907e0c105e2862540a8a73f2f38395
Dennis Moy <dennis@datatech.ws>
2018-06-05 01:49:29 -0400

    Add SQL generator functionality.

--- maingernerator.py
+++ maingernerator.py
@@ -3,10 +3,22 @@ from faker import Faker
 fake = Faker()
 
 Names = []
+filename = 'output.sql'
 
 for x in range(0,20):
     Names.append(fake.name())
 
 print(Names)
+
+header = 'INSERT INTO PERSON ( ID, NAME ) VALUES '
+
+with open(filename, 'w') as file:
+    file.write(header)
+
+    for index, name in enumerate(Names):
+        file.write("( " + str(index) + ", " + name + " )")
+
+        if index != len(Names) - 1:
+            file.write(", \n")
 
 # dennis is a potatoe
